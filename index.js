a04ef9aeb4d54c1c0a539b059a3c77d548bd13ae
invector4ik002 <Vedoone@mail.ru>
2020-07-01 14:48:15 +0700

    add

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json({ extended: true }));
+
+app.use('/api/auth', require('./routes/auth.routes'));
+// app.use('/api/link', require('./routes/link.routes')); // подключаем мидлвр обработка API запросов с ФРОНТА с маршрутизатором link.routes(логикой отправок и запросов)
+// app.use('/t', require('./routes/redirect.routes'));
+
+const PORT = config.get('port') || 4000;
+/**
+ * Функция подключения 
+ * "mongoUri": "mongodb+srv://MikDev:123MikDev@cluster0-egprj.mongodb.net/app",
+ */
+async function start() {
+   try {
+      await mongoose.connect(config.get('mongoUri'),{
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true,
+      })
+      app.listen(PORT, () => {
+         console.log(`Work: ...serwer ${PORT}`)
+      })
+   } catch (err) {
+      process.exit(1)
+   }
+}
+start();
